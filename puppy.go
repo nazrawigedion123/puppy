313f1adac4972dd21d88c6595bf5c30e744e61b6
nazri <nazrawigedion23@gmail.com>
2025-01-16 23:14:23 +0300

    tenth

--- puppy.go
+++ puppy.go
@@ -17,12 +17,12 @@ func BigBark() string{
 func BigBarks() string{
 	return dog.WhenGrownUp(Barks())
 }
-func Form11(){
+func Form11() string{
 	return "Im'from version 1.1.0"
 }
-func Form12(){
+func Form12() string{
 	return "I'm from version1.2.0"
 }
-func Form13(){
+func Form13() string{
 	return "I'm from version 1.3.0"
 }
